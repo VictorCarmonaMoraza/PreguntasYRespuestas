ba916b173eb66ce23c95cd14000577bb907ad872
Victor <VictorCarmona9035@hotmail.com>
2022-11-14 19:59:19 +0100

    21-Cambiar Password ts

--- src/app/components/inicio/dashboard/cambiar-password/cambiar-password.component.ts
+++ src/app/components/inicio/dashboard/cambiar-password/cambiar-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cambiar-password',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CambiarPasswordComponent implements OnInit {
 
-  constructor() { }
+cambiarPassword:FormGroup
+
+  constructor(private fb:FormBuilder) {
+    this.cambiarPassword=this.fb.group({
+      passwordAnterior:['',Validators.required],
+      nuevaPassword:['',[Validators.required],Validators.minLength(4)],
+      confirmPassword:['']
+    },{validator:this.checkPassword});
+   }
 
   ngOnInit(): void {
+  }
+
+  checkPassword(group: FormGroup): any {
+    const pass = group.controls['passwordAnterior'].value;
+    const confirmPass = group.controls['confirmPassword'].value;
+    return pass === confirmPass ? null : { notSame: true };
+  }
+
+  guardarPassword(){
+    console.log(this.cambiarPassword);
   }
 
 }
