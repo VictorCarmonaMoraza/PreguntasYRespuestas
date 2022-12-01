529874a3ce92414ea1fab77073b7464c5d8b3b46
Victor <VictorCarmona9035@hotmail.com>
2022-12-01 21:46:53 +0100

    39-Nueva Pregunta Component

--- src/app/components/inicio/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/nueva-pregunta/nueva-pregunta.component.ts
+++ src/app/components/inicio/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/nueva-pregunta/nueva-pregunta.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Pregunta } from 'src/app/models/pregunta';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nueva-pregunta',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevaPreguntaComponent implements OnInit {
 
-  constructor() { }
+  public nuevaPregunta: FormGroup;
+  pregunta: Pregunta;
+  rtaCorrecta = 0;
+
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService) {
+      this.nuevaPregunta = this.fb.group({
+        titulo:['', Validators.required],
+        respuestas:this.fb.array([])
+      });
+    }
 
   ngOnInit(): void {
   }
