7a09aeba393dba81a84510236a3205a287a69d95
Victor <VictorCarmona9035@hotmail.com>
2022-12-05 21:33:40 +0100

    54-Dashboard - Servicio GetListCuestionarioByUser

--- src/app/components/inicio/dashboard/cuestionarios/cuestionarios.component.ts
+++ src/app/components/inicio/dashboard/cuestionarios/cuestionarios.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../../../services/login.service';
+import { CuestionarioService } from '../../../../services/cuestionario.service';
+import { ToastrService } from 'ngx-toastr';
+import { Cuestionario } from 'src/app/models/cuestionario';
 
 @Component({
   selector: 'app-cuestionarios',
@@ -9,15 +12,30 @@ import { LoginService } from '../../../../services/login.service';
 export class CuestionariosComponent implements OnInit {
 
   public nombreUsuario:string;
+  public listCuestionarios:Cuestionario[]=[];
 
-  constructor(private loginService:LoginService) { }
+  constructor(
+    private loginService:LoginService,
+    private cuestionarioService:CuestionarioService,
+    private toastr:ToastrService) { }
 
   ngOnInit(): void {
     this.getNombreUsuario();
+    this.getCuestionarios();
   }
 
   getNombreUsuario():void{
     this.nombreUsuario =  this.loginService.getTokendDecoded().sub;
     //console.log(this.loginService.getTokendDecoded());
+  }
+
+  getCuestionarios():void{
+    this.cuestionarioService.getListCuestionario()
+    .subscribe(data=>{
+      console.log(data);
+      this.listCuestionarios = data;
+    }, error=>{
+      console.log(error);
+    })
   }
 }
