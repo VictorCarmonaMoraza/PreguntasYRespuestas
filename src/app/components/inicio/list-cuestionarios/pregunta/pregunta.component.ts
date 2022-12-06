4b545bdf413d14c440e7bba0b918588b1f68753c
Victor <VictorCarmona9035@hotmail.com>
2022-12-06 21:45:03 +0100

    64-PreguntaComponent - GetCuestionario

--- src/app/components/inicio/list-cuestionarios/pregunta/pregunta.component.ts
+++ src/app/components/inicio/list-cuestionarios/pregunta/pregunta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RespuestaCuestionarioService } from '../../../../services/respuesta-cuestionario.service';
+import { CuestionarioService } from '../../../../services/cuestionario.service';
 
 @Component({
   selector: 'app-pregunta',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreguntaComponent implements OnInit {
 
-  constructor() { }
+  public idCuestionario: number;
+
+  constructor(
+    private respuestaCuestionarioService: RespuestaCuestionarioService,
+    private cuestionarioService: CuestionarioService
+  ) { }
 
   ngOnInit(): void {
+    this.idCuestionario = this.respuestaCuestionarioService.idCuestionario;
+    //console.log(this.respuestaCuestionarioService.idCuestionario);
+    this.getCuestionario();
+  }
+
+  //Obtenemos el cuestionario por su id
+  getCuestionario(): void {
+    this.cuestionarioService.getCuestionario(this.idCuestionario)
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 
 }
