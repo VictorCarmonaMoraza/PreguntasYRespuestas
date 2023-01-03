0f07405d70f708cb82af0e73be10b4955cd13cef
Victor <VictorCarmona9035@hotmail.com>
2023-01-03 14:03:28 +0100

    72-RespuestaCuestionario TS

--- src/app/components/inicio/list-cuestionarios/respuesta-cuestionario/respuesta-cuestionario.component.ts
+++ src/app/components/inicio/list-cuestionarios/respuesta-cuestionario/respuesta-cuestionario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cuestionario } from '../../../../models/cuestionario';
+import { RespuestaCuestionarioService } from '../../../../services/respuesta-cuestionario.service';
 
 @Component({
   selector: 'app-respuesta-cuestionario',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RespuestaCuestionarioComponent implements OnInit {
 
-  constructor() { }
+  public cuestionario: Cuestionario;
+  public respuestaUsuario: number[] = [];
+
+  constructor(
+    private respuestaCuestionarioService: RespuestaCuestionarioService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    if(this.respuestaCuestionarioService.idCuestionario ==null){
+      this.router.navigate(['/inicio']);
+    }
+    else{
+      this.cuestionario = this.respuestaCuestionarioService.cuestionario;
+      this.respuestaUsuario = this.respuestaCuestionarioService.respuestas;
+    console.log(this.cuestionario);
+    console.log(this.respuestaUsuario);
+
+    }
   }
 
 }
